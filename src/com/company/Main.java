9f79383f1a1d9b54992f19f4907c05144a492857
Felipe <luiz.cruz@dock.tech>
2022-04-12 09:29:09 -0300

    Arraylist sem padrão Poo + foreachg

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,13 +10,18 @@ public class Main {
          String id = "125.855.999-13";
          String years = "28";
 
-
+        //CRIAÇÃO DA LISTA SEM POO
+        //CRIANDO AS VARIAVEIS E CONSTANTES :NAME,ID,YEARS.
+        //EXECUTANDO O ARRAYLIST
+        //ADD DENTRO DA LISTA PEOPLE QUE FOI CRIADA COM OS VALORES QUE FORAM DADOS
         ArrayList<String>people = new ArrayList<>();
 
         people.add(name);
         people.add(id);
         people.add(years);
 
+        //COMO CRIAR O FOR EACH
+        //APELIDO IDENTIFY E BUSCANDO DENTRO DE PEOPLE OQUE VAI SER LISTADO DENTRO DO PRINTLN
 
         for (String identify : people){
             System.out.println(identify);
